48886459d8a88908f5b5631c82a77eb53458ef8c
TrvMarco <trevisanello.marco@gmail.com>
2022-06-22 16:35:06 +0200

    Aggiunto valiodazione parametri in entrata

--- index.php
+++ index.php
@@ -26,3 +26,36 @@ $matches = [
 for($i = 0; $i < count($matches); $i++){
     echo $matches[$i]['team_one'].' - '.$matches[$i]['team_two'].' | '.$matches[$i]['team_one_points'].' - '.$matches[$i]['team_two_points']."<br>";
 }
+
+echo '<br><br><br>';
+
+//     SNACK 2 
+
+$name = $_GET["name"];
+$email = $_GET["email"];
+$age = $_GET["age"];
+
+$response = "";
+
+if(strlen($name) > 3 && filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($age, FILTER_VALIDATE_INT)){
+    $response = "Accesso consentito";
+}else{
+    $response = "Accesso negato";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks</title>
+</head>
+<body>
+    <p><strong>Nome</strong>: <?php echo $name?></p>
+    <p><strong>Email</strong>: <?php echo $email?></p>
+    <p><strong>Età</strong>: <?php echo $age?></p>
+    <p><strong>Autorizzazione: </strong><?php echo $response?></p>
+</body>
+</html>
